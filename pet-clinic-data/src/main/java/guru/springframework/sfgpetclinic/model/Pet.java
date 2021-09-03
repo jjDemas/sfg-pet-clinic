dcee400399632cf4fcdedbf2efeaecfc933fd5a8
jjDemas <jjdemason@comcast.net>
2021-09-03 17:34:22 -0400

    Added base entity. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends Person{
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDate birthday;
